df6f4d12687b6f3a66e90162e794641dcc13eb1b
timelineju <geminitestyong@gmail.com>
2026-08-18 04:20:26 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -44,7 +44,7 @@ module.exports = async (req, res) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                model: "llama3-8b-8192",
+                model: "llama-3.3-70b-versatile",
                 messages: [
                     { 
                         role: "system", 
